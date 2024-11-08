31d446e25fd2d1b7dd0135c02db7eabea5fe2f39
Alokcseit <swainalokkumar25@gmail.com>
2024-11-08 21:52:40 +0530

    useEffectfirstCommit

---
+++ src/New.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { useState,useEffect } from "react";
+function New(){
+ const[users,setUsers]=useState([])
+ useEffect(()=>{
+    axios.get()
+ },[])
+
+    return(
+        <>
+        <div>hello</div>
+        </>
+    )
+}
+export default New;
